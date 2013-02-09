427d7ab75dc722b1aee2949dd49623760b9baa96
Chris <cdesch@gmail.com>
2013-02-09 16:51:10 -0500

    Test Script
    Signed-off-by: Chris <cdesch@gmail.com>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ MEGA Model Optimization Package for OS X
 
 [MEGA written by Hal Daume III](http://www.cs.utah.edu/~hal/megam/)
 
-
-
-
-Using OCAML 4.0 from HomeBrew
+Dependancy's 
+	- OCAML 4.0 from HomeBrew
+	- OS X Lion 10.7
+	- 
--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,12 @@
 #Tests for MEGAM
 
 #Regular
-../megam -maxi 10 binary small2 > weights_b
+#../megam -maxi 10 binary small2 > weights_b 
 
 #Optimal
 #../megam.opt -maxi 10 binary small2 > weights_b
+
+
+#open in 32 bit
+
+#arch -i386 ../megam -maxi 10 binary small2 > weights_b 
